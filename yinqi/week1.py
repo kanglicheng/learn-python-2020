ce5d5c4e0ae3983a7c2c4ea303483f764c0e2a6a
Yinqi <yinqiz@usc.edu>
2020-12-12 15:52:11 -0800

    update on question 1

---
+++ yinqi/week1.py
@@ -0,0 +1,32 @@
+"""
+Chooses a random integer in [0, 100]. Asks user to enter a guess,
+terminates only when user guesses correctly
+"""
+import random
+
+
+def guessing_game():
+    number = random.randint(0, 10)
+    while True:
+        guess = int(input("Please enter a number between 1 and 100 (inclusive)"))
+        if guess == number:
+            return "correct!"
+        elif guess > number:
+            print("too large!") 
+        else:
+            print("too small!")   
+
+print(guessing_game())
+
+
+"""
+implement sum function
+sum(a, b, c, d, ... m, n) = a+b+c+d+...+m+n
+"""
+
+
+def mysum(*numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
