736877ef5285b0c3340c181e3ae31fd2adca0c09
huoying831 <tuyue831@gmail.com>
2020-12-20 11:24:57 -0800

    week2

---
+++ paddy/week2.py
@@ -0,0 +1,12 @@
+def ubbi_dubbi():
+    vowel = ["a", "e", "i", "o", "u"]
+    inword = input("Please enter a word \n")
+    if inword[0].lower() in vowel:
+        print(inword+"way")
+
+    else:
+        temp = inword[1:len(inword)] + inword[0] + "ay"
+        print(temp)
+
+
+ubbi_dubbi()
