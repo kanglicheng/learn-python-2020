b45be61477f47d8045d95e96d41aa2950889c9ff
huoying831 <tuyue831@gmail.com>
2020-12-20 11:33:36 -0800

    week2

--- paddy/week2.py
+++ paddy/week2.py
@@ -1,4 +1,4 @@
-def ubbi_dubbi():
+def pig_latin():
     vowel = ["a", "e", "i", "o", "u"]
     inword = input("Please enter a word \n")
     if inword[0].lower() in vowel:
@@ -7,6 +7,20 @@ def ubbi_dubbi():
     else:
         temp = inword[1:len(inword)] + inword[0] + "ay"
         print(temp)
+
+
+# pig_latin()
+
+def ubbi_dubbi():
+    vowel = ["a", "e", "i", "o", "u"]
+    inword = input("Please enter a word \n")
+    outword = ""
+    for i in inword:
+        if i in vowel:
+            outword = outword + "ub" + i
+        else:
+            outword = outword + i
+    print(outword)
 
 
 ubbi_dubbi()
